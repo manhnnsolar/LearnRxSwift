cb9243bea7ac834b9def7213d5235291ff610d75
manhmoon <manhnn@solarapp.asia>
2023-02-23 16:06:37 +0700

    Create base project learn RxSwift

--- LearnRx/ViewController.swift
+++ LearnRx/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.learnObservableSequences()
     }
 
-
+    private func learnObservableSequences() {
+        let helloSequence = Observable.just("Hello Babay")
+        let fibonaciSequencu = Observable.from([0, 1, 1, 2, 3, 5, 8])
+        let dictSequence = Observable.from([1: "Come on", 2: "Boy"])
+        
+        
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '12.0'
+
+target 'LearnRx' do
+  use_frameworks!
+
+  pod 'RxSwift', '~> 5.1'
+  pod 'RxCocoa'
+
+end
